242a8bf029f4f719e2095efc1d019961478957c0
nkovalev <nkovalev@lvlmed.ru>
2023-12-23 02:53:27 +0300

    init

---
+++ microservice-study-getter/src/test/kotlin/com/kovalevnadev/microservicestudygetter/MicroserviceStudyGetterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kovalevnadev.microservicestudygetter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroserviceStudyGetterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
